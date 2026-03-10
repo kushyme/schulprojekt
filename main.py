bdcd88d2c23e6e1154da133a9031b21e81416884
Peters, Erik <Erik.Peters.ext+ewe@ewe.de>
2026-03-10 08:39:45 +0100

    first fetch

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("helloworld")
+import requests
+
+def fetch_data():
+    try:
+        results = []
+        response = requests.get("https://api.thecatapi.com/v1/images/search")
+        if response.status_code == 200:
+            results = response.json()
+        else:
+            print(f"error fetching data: {response.status_code}")
+        print(results)
+    except requests.RequestException as e:
+        raise RuntimeError(f"Network error: {e}")
+
+fetch_data()
