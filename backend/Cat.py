fbdc38a1086d4ccf4af970c16f8d260c97784970
Peters, Erik <Erik.Peters.ext+ewe@ewe.de>
2026-03-11 12:22:03 +0100

    .

--- backend/Cat.py
+++ backend/Cat.py
@@ -1,5 +1,5 @@
 class Cat():
-    def __init__(self, limit=1, page=0, order="ASC", has_breeds=0, breed_ids=None, category_ids=None, sub_id=None):
+    def __init__(self, limit: int=1, page: int=0, order: str="ASC", has_breeds: int=0, breed_ids: str=None, category_ids: int=None, sub_id: str=None):
         self.limit = limit
         self.page = page
         self.order = order
@@ -7,3 +7,17 @@ class Cat():
         self.breed_ids = breed_ids
         self.category_ids = category_ids
         self.sub_id = sub_id
+        self.id = None
+        self.width = None
+        self.height = None
+        self.url = None
+        self.breeds = [{
+            self.weight: { self.imperial: str, self.metric: str },
+            self.id: str,
+            self.temperament: str,
+            self.origin: str,
+            self.country_codes: str,
+            self.country_code: str,
+            self.life_span: str,
+            self.wikipedia_url: str
+        }]
