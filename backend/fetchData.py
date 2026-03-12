bba31de45d4c044c089bf0c3360192f5d84f494a
zerroxxx9 <190294721+zerroxxx9@users.noreply.github.com>
2026-03-12 10:03:43 +0100

    cleanup

--- backend/fetchData.py
+++ backend/fetchData.py
@@ -1,10 +1,9 @@
 import requests
-from Cat import Cat
 
-def fetch_data(cat):
+def fetch_data():
     try:
         results = []
-        response = requests.get(f"https://api.thecatapi.com/v1/images/search?limit={cat.limit}&page={cat.page}&order={cat.order}&has_breeds={cat.has_breeds}&breed_ids={cat.breed_ids}&category_ids={cat.category_ids}&sub_id={cat.sub_id}")
+        response = requests.get(f"")
         if response.status_code == 200:
             results = response.json()
         else:
@@ -12,7 +11,3 @@ def fetch_data(cat):
         print(results)
     except requests.RequestException as e:
         raise RuntimeError(f"Network error: {e}")
-
-cat = Cat()
-
-fetch_data(cat)
